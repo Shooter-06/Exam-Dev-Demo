be117aa951c339bec87938c971cf1b873959b976
Fiacre YIRB <82058038+Shooter-06@users.noreply.github.com>
2022-10-01 08:56:07 -0400

    connection created frag

--- app/src/main/java/com/example/examdemo/MainActivity.java
+++ app/src/main/java/com/example/examdemo/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.examdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1, b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().add(R.id.container,
+                        new FirstFragment()).commit();
+                b1.setVisibility(View.GONE);
+            }
+        });
     }
 }
